e433dfead8b703875bcad0fdbfabd2193804a47e
Shivasaravanan1 <ssaravana68@gmail.com>
2024-03-29 19:32:22 +0530

    Kafka Project

---
+++ src/main/java/com/apache/apachetutorial/ApachetutorialApplication.java
@@ -0,0 +1,13 @@
+package com.apache.apachetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachetutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApachetutorialApplication.class, args);
+	}
+
+}
